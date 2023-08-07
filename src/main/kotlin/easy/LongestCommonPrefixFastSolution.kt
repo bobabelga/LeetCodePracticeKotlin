3b6325ec3770ccb4933096f33a36f470eee9efb5
Boba Belga <belgacemabibakr@gmail.com>
2023-08-07 10:38:34 +0100

    14. Longest Common Prefix Fast Solution Kotlin

---
+++ src/main/kotlin/easy/LongestCommonPrefixFastSolution.kt
@@ -0,0 +1,21 @@
+package easy
+
+fun main() {
+    println(longestCommonPrefixFast(arrayOf("flower", "flow", "flight")))
+    println(longestCommonPrefixFast(arrayOf("dog", "racecar", "car")))
+//    val bo="Boba"
+//    println(bo.indexOf("o"))
+//    println(bo.indexOf("i"))
+}
+
+fun longestCommonPrefixFast(strs: Array<String>): String {
+    var prefix = ""
+    if (strs.isEmpty()) return prefix
+    prefix = strs[0]
+    for (i in 1..strs.size - 1) {
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
+        }
+    }
+    return prefix
+}
